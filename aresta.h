8cde58fa36e803fdeb0dcbeaaf33daaeb4908b71
Gabriel Duque Schiffner Oliveira <gabrielduqueschiffner@gmail.com>
2022-06-03 21:45:54 -0300

    criando alguns getters

---
+++ aresta.h
@@ -0,0 +1,17 @@
+#ifndef ARESTA_H_INCLUDED
+#define ARESTA_H_INCLUDED
+
+using namespace std;
+
+class aresta
+{
+private:
+
+
+public:
+  
+
+
+};
+
+#endif // ARESTA_H_INCLUDED
---
+++ grafo.h
@@ -0,0 +1,17 @@
+#ifndef GRAFO_H_INCLUDED
+#define GRAFO_H_INCLUDED
+
+using namespace std;
+
+class grafo
+{
+private:
+  
+
+public:
+  
+
+
+};
+
+#endif // GRAFO_H_INCLUDED
